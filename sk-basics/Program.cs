911c4184b9247664f7e49be25fe52141ed15af02
Narsimha Rao Yedupati <nyedupati@microsoft.com>
2024-02-06 15:14:29 +0530

    Migrated to SK 1.3.0

--- sk-basics/Program.cs
+++ sk-basics/Program.cs
@@ -1,15 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Microsoft.SemanticKernel;
-using Microsoft.SemanticKernel.Orchestration;
 
 #region Using Azure OpenAI Chat Completion Service
 // For pre-requisites and Azure Open AI setup instructions, see https://learn.microsoft.com/en-us/azure/ai-services/openai/quickstart?tabs=command-line%2Cpython&pivots=programming-language-studio#prerequisites
 string apiKey = "";
-string deploymentName = "";
+string deploymentName = "gpt-35-turbo";
 string endpoint = "";
 
-var kernelBuilder = new KernelBuilder().
-    WithAzureOpenAIChatCompletionService(deploymentName, endpoint, apiKey);
+var kernelBuilder = Kernel.CreateBuilder().
+    AddAzureOpenAIChatCompletion(deploymentName, endpoint, apiKey);
 var kernel = kernelBuilder.Build();
 #endregion Using Azure OpenAI Chat Completion Service
 
@@ -26,14 +25,12 @@ var kernel = kernelBuilder.Build();
 
 string mathPrompt = "calculate mean and standard deviation from a  list of numbers.Numbers: ```{{$input}}``` .Do not format or add commentary to your response. No prose.The output should  only return the response as JSON object with keys mean and standardDeviation with no explanantion";
 //string mathPrompt = "Find all prime numbers from the provided input list of numbers.Numbers: ```{{$input}}``` .Do not format or add commentary to your response. No prose. ";
-var mathFn = kernel.CreateSemanticFunction(mathPrompt);
-ContextVariables variables = new ContextVariables
+var mathFn = kernel.CreateFunctionFromPrompt(mathPrompt);
+KernelArguments variables = new KernelArguments
 {
     { "input", "1,2,3,4,5,15,19,22,18." }
 };
-var output = await kernel.RunAsync(
-    variables,
-    mathFn);
+var output = await kernel.InvokeAsync(mathFn, variables);
 Console.WriteLine($"Output: {output.GetValue<string>()}");
 Console.ReadLine();
 Console.WriteLine("Press any key to exit...");
--- sk-plugin/Program.cs
+++ sk-plugin/Program.cs
@@ -1,15 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Microsoft.SemanticKernel;
-using Microsoft.SemanticKernel.Orchestration;
 
 #region Using Azure OpenAI Chat Completion Service
 // For pre-requisites and Azure Open AI setup instructions, see https://learn.microsoft.com/en-us/azure/ai-services/openai/quickstart?tabs=command-line%2Cpython&pivots=programming-language-studio#prerequisites
 string apiKey = "";
-string deploymentName = "";
+string deploymentName = "gpt-35-turbo";
 string endpoint = "";
 
-var kernelBuilder = new KernelBuilder().
-    WithAzureOpenAIChatCompletionService(deploymentName, endpoint, apiKey);
+var kernelBuilder = Kernel.CreateBuilder()
+    .AddAzureOpenAIChatCompletion(deploymentName, endpoint, apiKey);
 var kernel = kernelBuilder.Build();
 #endregion Using Azure OpenAI Chat Completion Service
 
@@ -20,23 +19,25 @@ var kernel = kernelBuilder.Build();
 //var kernelBuilder = new KernelBuilder().
 //    WithOpenAIChatCompletionService(model, apiKey);
 #endregion Using OpenAI Chat Completion Service
-var pluginsDirectory = Path.Combine(Directory.GetCurrentDirectory(), "Plugins");
+var pluginsDirectory = Path.Combine(Directory.GetCurrentDirectory(), "Plugins/CoursePlan");
 
-var courseModulePlugin= kernel.ImportSemanticFunctionsFromDirectory(pluginsDirectory, "CoursePlan");
+var courseModulePlugin= kernel.ImportPluginFromPromptDirectory(pluginsDirectory, "CoursePlan");
 
 
-var variables = new ContextVariables();
-variables.Set("topic", "OpenAI");
+KernelArguments variables = new KernelArguments
+{
+    { "topic", "OpenAI." }
+};
 
-var title = await kernel.RunAsync(variables, courseModulePlugin["Title"]); 
+var title = await kernel.InvokeAsync( courseModulePlugin["Title"],variables); 
 Console.WriteLine(title);
-variables.Set("title", title.GetValue<string>());
+variables.Add("title", title.GetValue<string>());
 
-var chapters = await kernel.RunAsync(variables, courseModulePlugin["Chapters"]);
+var chapters = await kernel.InvokeAsync(courseModulePlugin["Chapters"],variables);
 Console.WriteLine(chapters);
-variables.Set("chapters", chapters.GetValue<string>());
+variables.Add("chapters", chapters.GetValue<string>());
 
-var studyPlan = await kernel.RunAsync(variables, courseModulePlugin["Plan"]);
+var studyPlan = await kernel.InvokeAsync(courseModulePlugin["Plan"], variables);
 Console.WriteLine(studyPlan);
 
 Console.ReadLine();
